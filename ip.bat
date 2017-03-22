fbd1cff45e45b91dd2a45508913ad604f4560152
ArvidEnglund <ArvidEnglund89@gmail.com>
2017-03-22 15:40:14 +0100

    IP bat fil
    För att få lite info om din dator

---
+++ ip.bat
@@ -0,0 +1,9 @@
+ECHO OFF 
+ECHO As a network admin, I’m getting tired of having to type these commands in! Hopefully, this saves me some time in the long run. 
+IPCONFIG /ALL 
+PAUSE 
+SYSTEMINFO
+PAUSE
+PING www.google.com 
+ECHO All done pinging Google. 
+PAUSE
